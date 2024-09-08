de80c334bfc166af1d7227ab611a433de3781704
Muhammad Hasbi <hasbi@akasia.id>
2024-09-09 01:21:11 +0800

    fix edge function crash

--- api/index.ts
+++ api/index.ts
@@ -1,10 +1,11 @@
 import { Hono } from "hono";
 import { handle } from "hono/vercel";
 
+export const runtime = "edge";
+
 const app = new Hono().basePath("/api");
 
 app.get("/", (c) => {
-  c.status(200);
   return c.json({ message: "Hello" }, 200);
 });
 
