7efd970e66760966f9050c3d8b2eb0690e9d528d
Muhammad Hasbi <hasbi@akasia.id>
2024-11-04 05:37:02 +0800

    bugfix deployment on server

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
-import { handle } from "hono/vercel";
-import { app } from "../src";
+import { Hono } from 'hono'
+import { handle } from 'hono/vercel'
+
+const app = new Hono().basePath('/api')
+
+app.get('/', (c) => {
+  return c.json({ message: "Congrats! You've deployed Hono to Vercel" })
+})
 
 const handler = handle(app);
 
--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import { Hono } from "hono";
-
-const app = new Hono().basePath("/api");
-
-app.get("/", (c) => {
-  return c.json({ message: "Congrats! You've deployed Hono to Vercel" });
-});
-
-export { app };
